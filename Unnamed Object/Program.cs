9b146b8b15146fcb9dccc2d536cea1890ba2a33f
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-29 02:08:00 +0530

    Add project files.

---
+++ Unnamed Object/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Unnamed_Object
+{
+    class C1
+    {
+        public void show()
+        {
+            Console.WriteLine("hello :");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new C1().show();
+            new C1().show();
+        }
+    }
+}
